1a5391a704de38099add2dd324ac9076816861e9
Justyna Ciarachowicz <j.ciarachowicz@gmail.com>
2020-07-27 10:20:14 +0200

    Updates

---
+++ precache-manifest.9188254496269c4fe070185300232480.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e35c9ba256594720d9e7f43bf5a834d",
+    "url": "/To-do-list-react/index.html"
+  },
+  {
+    "revision": "7d03f384f6ce89ff1a51",
+    "url": "/To-do-list-react/static/css/main.ba8fd6f5.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/To-do-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d03f384f6ce89ff1a51",
+    "url": "/To-do-list-react/static/js/main.09dafbad.chunk.js"
+  },
+  {
+    "revision": "5a9c30f72c758a6ad52a",
+    "url": "/To-do-list-react/static/js/runtime-main.710af20f.js"
+  }
+]);
