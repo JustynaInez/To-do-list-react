05d04dd8f68c5c256edae4046d315e5847f1d3be
Justyna Ciarachowicz <j.ciarachowicz@gmail.com>
2020-07-27 09:42:04 +0200

    container & heder to react

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Form from "./Form";
 import List from "./List";
 import Buttons from "./buttons";
 import Section from "./Section";
+import Header from "./Header";
+import Container from "./Container";
 
 const tasks = [
   { id: 1, content: "fdfd", done: true },
   { id: 2, content: "fdwe", done: true }
+
 ];
 const hideAllTasksDone = false;
 
 function App() {
   return (
-    <body>
-      <h1 className="Headline">Lista Zadań</h1>
+    <Container>
+     <Header title="Lista Zadań" />
       <Section
         title="Dodaj nowe zadanie"
         body={<Form />}
@@ -23,7 +26,7 @@ function App() {
       body ={<List tasks={tasks} hideAllTasksDone={hideAllTasksDone}/>}
       extraContent = {<Buttons tasks={tasks} hideAllTasksDone={hideAllTasksDone}/>}
       />
-    </body>
+    </Container>
   );
 }
 
---
+++ src/Container/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Container = ({children}) => (
+    <body>
+        {children}
+    </body>
+);
+
+export default Container
