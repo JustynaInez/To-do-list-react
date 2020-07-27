43174b8d89bb3c3b06afce20eec106006a07536c
Justyna Ciarachowicz <j.ciarachowicz@gmail.com>
2020-07-27 09:27:34 +0200

    section to react

--- src/App.js
+++ src/App.js
@@ -2,35 +2,28 @@ import React from 'react';
 import Form from "./Form";
 import List from "./List";
 import Buttons from "./buttons";
+import Section from "./Section";
 
 const tasks = [
-  {id:1, content: "fdfd", done: true },
-  {id:2, content: "fdwe", done: true }
+  { id: 1, content: "fdfd", done: true },
+  { id: 2, content: "fdwe", done: true }
 ];
 const hideAllTasksDone = false;
 
 function App() {
   return (
-      <body>
-
-        <h1 className="Headline">
-          Lista Zadań
-</h1>
-        <section className="section">
-          <h2 className="section__headline">Dodaj nowe zadanie</h2>
-
-         <Form />
-          </section>
-
-          <section className="section ">
-          <h2 className="section__headline section__flex" >Lista zadań  
-          <Buttons tasks={tasks} hideAllTasksDone= {hideAllTasksDone}/> 
-          </h2>
-            <List tasks={tasks} hideAllTasksDone= {hideAllTasksDone} />
-          </section>
-          </body>
-
-        
+    <body>
+      <h1 className="Headline">Lista Zadań</h1>
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
+      <Section
+        title="Lista zadań"
+      body ={<List tasks={tasks} hideAllTasksDone={hideAllTasksDone}/>}
+      extraContent = {<Buttons tasks={tasks} hideAllTasksDone={hideAllTasksDone}/>}
+      />
+    </body>
   );
 }
 
--- src/Section/index.js
+++ src/Section/index.js
@@ -1,3 +1,13 @@
 import React from "react";
 import "./style.css"
 
+const Section = ({title, body, extraContent}) => (
+    <section className="section ">
+    <h2 className="section__headline section__flex" >{title}
+      {extraContent}
+    </h2>
+    {body}
+  </section>
+)
+
+export default Section ;
